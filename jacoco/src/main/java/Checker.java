edc334ab617a96346edd9a5af63738ab02c220cf
ZanderKoch <89077492+ZanderKoch@users.noreply.github.com>
2021-09-09 10:38:23 +0200

    implemented checker

--- jacoco/src/main/java/Checker.java
+++ jacoco/src/main/java/Checker.java
@@ -8,6 +8,30 @@
  *
  * @author Zanders andra acc
  */
-public class Checker {
+public class Checker{
+    
+    ///////////
+    //methods//
+    public boolean isPalindrome(String input){
+    
+        if(input == null){
+        throw new IllegalArgumentException("argument can't be null");
+        }
+        
+        if(input.equals(reverse(input))){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    private String reverse(String input){
+        String reverse = "";
+        
+       for(int i = input.length() - 1; i >= 0; i--){
+           reverse = += input.charAt(i);
+       }
+    }
     
 }
